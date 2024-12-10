a88f80e829f06a1af369d4cacb3e0c019570c9fa
DHANUSH R <139610053+DHANUSHPLR@users.noreply.github.com>
2024-12-10 22:23:07 +0530

    Add files via upload

---
+++ employment-agreements/src/main/java/com/example/employment_agreements/EmploymentAgreementsApplication.java
@@ -0,0 +1,13 @@
+package com.example.employment_agreements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploymentAgreementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmploymentAgreementsApplication.class, args);
+	}
+
+}
---
+++ employment-agreements/src/test/java/com/example/employment_agreements/EmploymentAgreementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employment_agreements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmploymentAgreementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
